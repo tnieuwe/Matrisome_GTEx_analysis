510b5a441fd7ca1251a7f5edf0cac6a67aa24857
tnieuwe <40697504+tnieuwe@users.noreply.github.com>
2021-03-09 13:39:14 -0500

    Added the general limma analysis used on the MARCC cluster

---
+++ GTEx_age_sex_analysis/general_limma_age_sex_analysis.R
@@ -0,0 +1,103 @@
+## General limma + edgeR analysis
+library(tidyverse)
+library(limma)
+library(edgeR)
+
+
+## Adding a function to make SAMPID SUBJID
+## SAMPID to SUBJID function
+SAMPID_to_SUBJID <- function(sampids){
+    gregexpr("-" , "GTEX-1117F-0226-SM-5GZZ7")[[1]][[2]]
+    
+    greg_out <- gregexpr("-", sampids)
+    lapply_out <- lapply(greg_out, function(x){x[[2]] -1})
+    str_sub(sampids,1,lapply_out)
+}
+
+## Gene count location
+gene_count_location <- "GTEx_age_sex_analysis/input/gtex-gene-counts-v8-new.rda"
+## Subject data location
+subject_location <-  "GTEx_age_sex_analysis/input/gtex_phenotypes_v8.csv"
+## Matrisome data location
+matrisome_location <- "global_in/matrisome_hs_masterlist_EDIT_r.csv"
+## Age and sex output location
+ans_output <- "GTEx_age_sex_analysis/output/"
+## Current tiss
+cur_tiss <- "Nerve - Tibial"
+
+## Load in the read counts and other files
+## read counts, gene names, sample data
+#load(gene_count_location)
+## SUBJID data
+subj_dat <- read.csv(subject_location)
+## Matrisome data
+mat_dat <- read.csv(matrisome_location)
+colnames(mat_dat)[1] <- "Division" 
+
+## Filter mat_dat
+mat_dat_core <- filter(mat_dat, Division == "Core matrisome" | 
+                           Category == "ECM Regulators")
+
+
+##Filter to thyroid and remove low count genes
+ind <- stab$SMTSD %in% cur_tiss
+stab_filt <- stab[ind,]
+sub_dat <- dat[,ind]
+
+## Filter out genes with low expression
+keep.exprs <- filterByExpr(sub_dat)
+sub_dat <- sub_dat[keep.exprs,]
+gtab_filt <- gtab[keep.exprs,]
+
+
+stab_filt$SUBJID <- SAMPID_to_SUBJID(stab_filt$SAMPID)
+samp_n_subj <- left_join(stab_filt, subj_dat)
+## Scale the age
+samp_n_subj$SCALEAGE <- scale(samp_n_subj$AGE)
+
+
+
+## Run Limma analysis
+if (length(unique(samp_n_subj$SEX)) > 1) {
+    design <- model.matrix(~samp_n_subj$SCALEAGE + samp_n_subj$SEX)
+} else{
+    design <- model.matrix(~samp_n_subj$SCALEAGE)
+}
+## Normalize with voom
+voom_dat <- voom(sub_dat, design = design)
+mmp3_dt <- voom_dat$E[ind,]
+t_mmp3_dt <- t(mmp3_dt)
+plot(t_mmp3_dt[,1], t_mmp3_dt[,2])
+cor.test(t_mmp3_dt[,1], t_mmp3_dt[,2])
+## Quick test
+ind <- gtab_filt$gene_name %in% c("MMP3", "HBB")
+
+## Run lmFit to fit the model
+vfit <- lmFit(voom_dat, design)
+## Run eBayes to generate statistics and DE results.
+efit <- eBayes(vfit)
+
+### Clean up data
+## Pull out the results of SCALEDAGE
+age_results_limma <- topTable(efit,coef=2, sort = "none", n =Inf, adjust.method = "holm")
+age_results_mat <- age_results_limma %>%
+    rownames_to_column(var = "gene_id") %>%
+    left_join(., gtab_filt) %>%
+    filter(gene_name %in% mat_dat_core$Gene.Symbol)
+
+## Pull out the results of SEX
+if (length(unique(samp_n_subj$SEX)) > 1) {
+    sex_results_limma <- topTable(efit,coef=3, sort = "none", n =Inf, adjust.method = "holm")
+    sex_results_mat <- sex_results_limma %>%
+        rownames_to_column(var = "gene_id") %>%
+        left_join(., gtab_filt) %>%
+        filter(gene_name %in% mat_dat_core$Gene.Symbol)
+}
+
+
+
+## save output
+write.csv(age_results_mat, paste0(ans_output, "limma_thyroid_res_age.csv"),row.names = F)
+if (length(unique(samp_n_subj$SEX)) > 1) {
+    write.csv(sex_results_mat, paste0(ans_output, "limma_thyroid_res_sex.csv"),row.names = F)
+}
